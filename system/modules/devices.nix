b405b7fac2231a4cdfb2cda9ba8ec8d0f8e6cc05
Dimluar <148585902+Dimluar@users.noreply.github.com>
2026-03-30 15:30:59 +0200

    Add bluetooth support

--- system/modules/devices.nix
+++ system/modules/devices.nix
@@ -17,4 +17,27 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
+
+  # Enable bluetooth
+  hardware.bluetooth = {
+    enable = true;
+    # powerOnBoot = true;
+    settings = {
+      General = {
+        # Shows battery charge of connected devices on supported
+        # Bluetooth adapters. Defaults to 'false'.
+        Experimental = true;
+        # When enabled other devices can connect faster to us, however
+        # the tradeoff is increased power consumption. Defaults to
+        # 'false'.
+        # FastConnectable = true;
+      };
+      Policy = {
+        # Enable all controllers when they are found. This includes
+        # adapters present on start as well as adapters that are plugged
+        # in later on. Defaults to 'true'.
+        AutoEnable = true;
+      };
+    };
+  };
 }
