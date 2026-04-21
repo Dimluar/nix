2b881dd362ee97834926587131b495158f7777f8
Dimluar <148585902+Dimluar@users.noreply.github.com>
2026-04-21 17:48:58 +0200

    Activate Magic SysRq

--- system/modules/managers.nix
+++ system/modules/managers.nix
@@ -4,6 +4,9 @@
   boot.loader.timeout = null;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Activate SysRq
+  boot.kernel.sysctl."kernel.sysrq" = 502;
+
   # Enable display manager
   services.displayManager.sddm.enable = true;
 
